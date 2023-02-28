9a00b55bc48b39dcbb073d73c8f7993601632e25
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-02-28 13:39:40 -0600

    made changes in server js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const connection = mysql.createConnection({
   user: 'root',
   password: 'Ilovemyself',
   database: 'employeeTracker_db',
-});
+},
+console.log(`Connected to the employeeTracker_db database.`)
+);
 
 // Connect to the DB
 connection.connect((err) => {
@@ -34,7 +36,8 @@ function startPrompt() {
     loop: false,
     choices: ["View all employees", "View all roles", "View all departments", "add an employee", "add a role", "add a department", "update role for an employee", "update employee's manager", "view employees by manager", "delete a department", "delete a role", "delete an employee", "View the total utilized budget of a department", "quit"]
   }]
-  
+
+  // inquirer promter
   inquier.prompt(startQuestion)
   .then(response => {
     switch (response.action) {
